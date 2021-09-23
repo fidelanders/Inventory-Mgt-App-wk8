dee7592c7db4d23b06b2580983a0a53aa69b9543
fidelanders <fidelanders@gmail.com>
2021-09-23 17:59:15 +0100

    update

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const path = require('path')
 app.use(express.static(path.join(__dirname , 'public'))) //portfolio page
 
 app.get('/name' , (req , res) => {
-   res.send(`Prosper Eravwuvieke`)
+   res.send(`Oogbeni Fidelis`)
 }) // prints name on the browser
 
 
 
-
-
 app.listen(port , () => {
-   console.log(`server is listening on port ${port}`)
+   console.log(`Server Ready http://localhost:${port}`)
 })
